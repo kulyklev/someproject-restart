3f79e512521f025dbeab82b6552a18e8314e8ef8
Lev <lev.kulik@computools.com>
2019-10-25 17:36:44 +0300

    add register page

--- frontend_src/src/views/Register.vue
+++ frontend_src/src/views/Register.vue
@@ -1,11 +1,76 @@
 <template>
-  <div class="about">
-    <h1>This is a register page</h1>
+  <div class="login">
+    <h1>This is a login page</h1>
+    <b-container>
+      <b-row>
+        <b-col cols="12" sm="6" offset-sm="3">
+          <b-card>
+            <b-card-text>
+              <b-container>
+                <b-form class="text-left">
+                  <b-form-group id="name-input-group"
+                                label="Name:"
+                                label-for="input-1">
+                    <b-form-input v-model="form.name"
+                                  id="name-input"
+                                  type="text"
+                                  placeholder="Enter your name"
+                                  required/>
+                  </b-form-group>
+
+                  <b-form-group id="email-input-group"
+                                label="Email address:"
+                                label-for="email-input">
+                    <b-form-input v-model="form.email"
+                                  id="email-input"
+                                  type="email"
+                                  placeholder="Enter your email"
+                                  required/>
+                  </b-form-group>
+
+                  <b-form-group id="password-input-group"
+                                label="Password:"
+                                label-for="password-input">
+                    <b-form-input v-model="form.password"
+                                  id="password-input"
+                                  type="password"
+                                  placeholder="Enter your password"
+                                  required/>
+                  </b-form-group>
+
+                  <b-form-group id="password-confirmation-input-group"
+                                label="Confirm your password:"
+                                label-for="password-confirm-input">
+                    <b-form-input v-model="form.passwordConfirmation"
+                                  id="password-confirm-input"
+                                  type="password"
+                                  placeholder="Confirm your password"
+                                  required/>
+                  </b-form-group>
+
+                  <b-button type="submit" variant="primary">Submit</b-button>
+                </b-form>
+              </b-container>
+            </b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Register',
+  name: 'register',
+  data() {
+    return {
+      form: {
+        name: '',
+        email: '',
+        password: '',
+        passwordConfirmation: '',
+      },
+    };
+  },
 };
 </script>
