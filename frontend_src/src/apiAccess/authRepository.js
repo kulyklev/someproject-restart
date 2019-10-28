c18beef93881696174ba68694c7554d04b072fb8
Lev <lev.kulik@computools.com>
2019-10-28 16:59:21 +0200

    add sending request from login form

--- frontend_src/src/apiAccess/authRepository.js
+++ frontend_src/src/apiAccess/authRepository.js
@@ -8,6 +8,11 @@ export default {
   },
 
   login(payload) {
-    return Repository.post('/login', payload);
+    const formData = new FormData();
+
+    formData.set('email', payload.email);
+    formData.set('password', payload.password);
+
+    return Repository.post('/login', formData);
   },
 };
--- frontend_src/src/views/Login.vue
+++ frontend_src/src/views/Login.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="login">
-    <h1>This is a login page</h1>
+    <h1>Login</h1>
     <b-container>
       <b-row>
         <b-col cols="12" sm="6" offset-sm="3">
           <b-card>
             <b-card-text>
               <b-container>
-                <b-form class="text-left">
+                <b-form class="text-left" @submit.prevent="onLogin">
                   <b-form-group id="email-input-group"
                                 label="Email address:"
                                 label-for="email-input">
                     <b-form-input v-model="form.email"
                                   id="email-input"
-                                  type="email"
-                                  placeholder="Enter your email"
-                                  required/>
+                                  :state="$v.form.email.$dirty ? !$v.form.email.$error : null"
+                                  aria-describedby="email-input-live-feedback"/>
+
+                    <b-form-invalid-feedback id="email-input-live-feedback">
+                      This is a required field and must be a valid email address.
+                    </b-form-invalid-feedback>
                   </b-form-group>
 
                   <b-form-group id="password-input-group"
@@ -24,8 +27,12 @@
                     <b-form-input v-model="form.password"
                                   id="password-input"
                                   type="password"
-                                  placeholder="Enter your password"
-                                  required/>
+                                  :state="$v.form.password.$dirty ? !$v.form.password.$error : null"
+                                  aria-describedby="password-input-live-feedback"/>
+
+                    <b-form-invalid-feedback id="password-input-live-feedback">
+                      This is a required field.
+                    </b-form-invalid-feedback>
                   </b-form-group>
 
                   <b-button type="submit" variant="primary">Submit</b-button>
@@ -40,17 +47,50 @@
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate';
+import { required, email as emailValidation } from 'vuelidate/lib/validators';
+import RepositoryFactory from '../apiAccess/repositoryFactory';
+
+const AuthRepository = RepositoryFactory.get('auth');
+
 export default {
   name: 'Login',
+  mixins: [validationMixin],
   data() {
     return {
       form: {
-        name: '',
         email: '',
         password: '',
-        passwordConfirmation: '',
       },
     };
+  },
+  validations: {
+    form: {
+      email: {
+        required,
+        emailValidation,
+      },
+      password: {
+        required,
+      },
+    },
+  },
+  methods: {
+    onLogin() {
+      this.$v.form.$touch();
+      if (this.$v.form.$invalid) {
+        return;
+      }
+
+      const data = AuthRepository.login(this.form);
+      data.then((response) => {
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.headers);
+        console.log(response.config);
+      });
+    },
   },
 };
 </script>
