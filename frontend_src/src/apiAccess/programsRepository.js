0d471d6823f981eda8c5ef575adcf72b07ff418e
Lev <lev.kulik@computools.com>
2019-10-31 12:46:22 +0200

    fix saving programs

--- frontend_src/src/apiAccess/programsRepository.js
+++ frontend_src/src/apiAccess/programsRepository.js
@@ -4,15 +4,14 @@ import Repository from './repository';
 
 export default {
   get() {
-    // TODO Get all programs
-    // return Repository.post('/register', formData);
+    return Repository.get('programs');
   },
 
   save(newProgram) {
     const formData = new FormData();
 
     formData.set('name', newProgram.name);
-    formData.set('program', newProgram.code);
+    formData.set('program', newProgram.program);
 
     return Repository.post('/programs', formData);
   },
@@ -22,7 +21,7 @@ export default {
     const formData = new FormData();
 
     formData.set('name', updatedProgram.name);
-    formData.set('program', updatedProgram.code);
+    formData.set('program', updatedProgram.program);
     formData.set('_method', 'PUT');
 
     return Repository.post(`/programs/${programId}`, formData);
